5a0a5b5137826b02daafdc652fd27f3adfe1f685
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-03-08 20:31:10 +0100

    are you very hungry? now you can eat even more! xD

--- src/damas/Damas.java
+++ src/damas/Damas.java
@@ -19,51 +19,70 @@ public class Damas implements Game {
     @Override
     public void startGame() {
         table = new DamasBoard();
-        int count = 0;
+        int count = 1;
         while (table.isGameOver() == null) {
-            table.printBoard();
             playerTurn((count++ % 2 == 1) ? p1 : p2);
         }
         table.printBoard();
-        System.out.println("Game Over");
+        System.out.println("Game Over\nWinner is: " + table.isGameOver() );
     }
 
     private void playerTurn(DamasPlayer player) {
-        ArrayList<int[]> movables = table.listOfActionables(player, false);
-        ListManip.printList(movables, true,1);
+        table.printBoard();
+        println("Player " + player.getIdQ() + " Turn.");
+        ArrayList<int[]> movables = table.listOfActionables(player);
+        if(movables.size() == 0) {
+            println("Player " + player.getIdQ() + " has no available moves. Check Bugs!");
+            return;
+        }
+        ListManip.printList(movables, true, 1);
         //pick piece
         int piece = 0;
         do {
             piece = scanInt("Move piece: ");
-        }while( piece < 1 || piece > movables.size());
-        int[] pieceCoords = movables.get(piece-1);
+        } while (piece < 1 || piece > movables.size());
+        int[] pieceCoords = movables.get(piece - 1);
 
         ArrayList<int[]> moves = table.listOfMoves(pieceCoords);
         ArrayList<int[]> attacks = table.listOfAttackMoves(pieceCoords);
-        ArrayList<int[]> movats = new ArrayList<>(moves);
+        ArrayList<int[]> movats = new ArrayList<>();
+        movats.addAll(moves);
         movats.addAll(attacks);
-
-        if(moves.size() > 0) {
-            println("Moves: ");
-            ListManip.printList(moves, true, 1);
-        }
-
-        if(attacks.size() > 0){
-            println("Attacks: ");
-            ListManip.printList(attacks,true, moves.size()+1);
-        }
-
-        //pick position to move piece to
-        int move = 0;
         do {
-            move = scanInt("Pick a move:");
-        }while(move < 1 || move >  movats.size() );
-        int[] destination = movats.get(move-1);
 
-        if(move < moves.size())
-            table.move(pieceCoords, destination);
-        else
-            table.eat(table.table, pieceCoords, destination);
+            if (moves.size() > 0) {
+                println("Moves: ");
+                ListManip.printList(moves, true, 1);
+            }
+
+            if (attacks.size() > 0) {
+                println("Attacks: ");
+                ListManip.printList(attacks, true, moves.size() + 1);
+            }
+
+            int move = 0;
+            do {
+                move = scanInt("Pick a move:");
+            } while (move < 1 || move > movats.size());
+            int[] destination = movats.get(move - 1);
+
+            if (move <= moves.size()) {
+                table.move(pieceCoords, destination);
+                break;
+            }
+            table.eat(pieceCoords, destination);
+            pieceCoords = destination;
+
+            attacks = table.listOfAttackMoves(pieceCoords);
+            moves.clear();
+            movats.clear();
+            movats.addAll(attacks);
+
+            if (attacks.size() > 0 )
+                table.printBoard();
+        } while (movats.size() > 0);
+
+
     }
 
 
