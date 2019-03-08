1654c22ad38f69ad03757e7be29c50add9145a41
fsancheztemprano <fsancheztemprano@users.noreply.github.com>
2019-03-08 17:59:07 +0100

    are you hungry? now you can eat! xD

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,15 @@ import proto.Game;
 //import lib.*;
 public class Main {
     public static void main(String[] args) {
-        String[] games = {"3 en Linea", "Damas"};
+        String[] games = {"Damas", "3 en linea"};
         String selection = (String)JOptionPane.showInputDialog(null, "Elige un juego", "Juegos Reunidos", 0, null, games, 0);
 
         Game game;
 
         switch (selection) {
+            case "Damas":
+                game = new Damas();
+                break;
             case "3 en Linea":
                 game = new TresEnLinea();
                 break;
