db8a106fc264c3cb4cbda18d09f2178d1e9386c7
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-03-11 13:13:36 +0100

    t

--- src/treslinea/TresEnLinea.java
+++ src/treslinea/TresEnLinea.java
@@ -4,6 +4,8 @@ import proto.Game;
 import proto.GamePane;
 import proto.SimplePlayer;
 
+import javax.swing.*;
+
 
 public class TresEnLinea implements Game {
 
@@ -41,10 +43,30 @@ public class TresEnLinea implements Game {
     public int[] enterCoords(SimplePlayer simplePlayer) {
         int x = 0, y = 0;
         do {
+
+
+        } while (x < 1 || x > 3 || y < 1 || y > 3);
+        return new int[]{x, y};
+    }
+/* consola
+    public void playerTurn(SimplePlayer simplePlayer) {
+        int[] coords;
+        boolean valid = false;
+        do {
+            coords = enterCoords(simplePlayer);
+            valid = game.validTurn(coords, simplePlayer);
+        } while (!valid);
+        game.doTurn(coords, simplePlayer);
+    }
+
+    public int[] enterCoords(SimplePlayer simplePlayer) {
+        int x = 0, y = 0;
+        do {
             System.out.println("SimplePlayer " + simplePlayer.getId() + " enter coords x , y: ");
             x = lib.Misc.IO.scanInt();
             y = lib.Misc.IO.scanInt();
         } while (x < 1 || x > 3 || y < 1 || y > 3);
         return new int[]{x, y};
     }
+    */
 }
